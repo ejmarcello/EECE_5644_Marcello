3af6f6e22ddac46658b53e9682482d424447200c
Ethan Marcello <ethanmarcello@gmail.com>
2022-04-12 17:01:05 -0400

    Create GMM data for Question 2

--- Assignment3/Assignment3.m
+++ Assignment3/Assignment3.m
@@ -286,16 +286,15 @@ clear all; close all;
 
 % Setup: create samples based on data distribution & keep track of
 % class labels.
-N = [100 200 500 1000 2000 5000 100000]; % numbers of samples for ith dataset
-n = 3; % dimensionality of the data vector
-sep = 1.25; % Data mean separation parameter
-C = 4; % Number of classes
-gmmParameters.priors = [0.25 0.25 0.25 0.25]; % equal priors
-% Construct data means by placing them on the corners of a cube
+N = [10 100 1000 10000]; % numbers of samples for ith dataset
+n = 2; % dimensionality of the data vector
+C = 4; % Number of classes/GMM components
+gmmParameters.priors = [0.2 0.1 0.3 0.4]; % different priors
+% Choose random mean vectors
 m1 = zeros(n,1); % mean vector for C=1. Length is same as n
-m2 = [sep 0 0]'; % mean vector for C=2
-m3 = [0 sep 0]'; % mean vector for C=3
-m4 = [0 0 sep]'; % mean vector for C=4
+m2 = [2 0]'; % mean vector for C=2
+m3 = [3 3]'; % mean vector for C=3
+m4 = [0.5 2]'; % mean vector for C=4
 gmmParameters.meanVectors = [m1 m2 m3 m4];
 
 % Now calculate Covariance matricies.
@@ -308,10 +307,10 @@ s2 = rand()*0.3;
 s3 = rand()*0.3;
 s4 = rand()*0.3;
 %Sigma = (I+sA)(I+sA)' with 0<s<<1
-Cov1 = 0.2.*(eye(3)+s1.*A1)*(eye(3)+s1.*A1)';
-Cov2 = 0.2.*(eye(3)+s2.*A2)*(eye(3)+s2.*A2)';
-Cov3 = 0.2.*(eye(3)+s3.*A3)*(eye(3)+s3.*A3)';
-Cov4 = 0.2.*(eye(3)+s4.*A4)*(eye(3)+s4.*A4)';
+Cov1 = 0.2.*(eye(n)+s1.*A1)*(eye(n)+s1.*A1)';
+Cov2 = 0.2.*(eye(n)+s2.*A2)*(eye(n)+s2.*A2)';
+Cov3 = 0.2.*(eye(n)+s3.*A3)*(eye(n)+s3.*A3)';
+Cov4 = 0.2.*(eye(n)+s4.*A4)*(eye(n)+s4.*A4)';
 gmmParameters.covMatrices(:,:,1) = Cov1;
 gmmParameters.covMatrices(:,:,2) = Cov2;
 gmmParameters.covMatrices(:,:,3) = Cov3;
@@ -319,40 +318,40 @@ gmmParameters.covMatrices(:,:,4) = Cov4;
 clear A1 A2 A3 A4 s1 s2 s3 s4;
 
 for i = 1:length(N)
-      % Using class priors to generate data x
-    x1 = randGaussian(N(i)*0.25,m1,Cov1); % C=1 data, Uses randGaussian function
-    x1(:,:,2) = ones(n,N(i)*0.25); % Add label to 3rd dimension of mtx
-    x2 = randGaussian(N(i)*0.25,m2,Cov2); % C=2 data, Uses randGaussian function
-    x2(:,:,2) = 2.*ones(n,N(i)*0.25); % Add label to 3rd dimension
-    x3 = randGaussian(N(i)*0.25,m3,Cov3); % C=3 data, first gaussian model
-    x3(:,:,2) = 3.*ones(n,N(i)*0.25); % Add label to 3rd dimension
-    x4 = randGaussian(N(i)*0.25,m4,Cov4); % C=3 data, second gaussian model
-    x4(:,:,2) = 4.*ones(n,N(i)*0.25); % Add label to 3rd dimension
+    % Using class priors to generate data x
+    x = [];
+    labels = [];
+    for c = 1:length(gmmParameters.priors)
+        % C=c data, Uses randGaussian function, and concatenates new data
+        % each loop.
+        data(c).x = randGaussian(N(i)*gmmParameters.priors(c),gmmParameters.meanVectors(:,c),gmmParameters.covMatrices(:,:,c)); 
+        data(c).labels = c.*ones(1,N(i)*gmmParameters.priors(c)); % Add label for data just generated.
+        x = [x data(c).x];
+        labels = [labels data(c).labels];
+    end
 
-    x = [x1(:,:,:) x2(:,:,:) x3(:,:,:) x4(:,:,:)]; 
-    % We know all C=1 data is in x1 and all C=2 data is in x2, etc.
-    labels = x(1,:,2);
-    x = x(:,:,1);
     dataset(i).x = x;
     dataset(i).labels = labels;
 end
 
-clear m1 m2 m3 m4 Cov1 Cov2 Cov3 Cov4;
+clear data x labels m1 m2 m3 m4 Cov1 Cov2 Cov3 Cov4;
 
-% Plot data in 3D to see level of overlap
+% Plot data in 2D to see level of overlap
 figure;
-plot3(x1(1,:)',x1(2,:)',x1(3,:)','r*')
+plot(dataset(3).x(1,1:200),dataset(3).x(2,1:200),'r*')
 hold on
-plot3(x2(1,:)',x2(2,:)',x2(3,:)','g*')
-plot3(x3(1,:)',x3(2,:)',x3(3,:)','c*')
-plot3(x4(1,:)',x4(2,:)',x4(3,:)','m*')
+plot(dataset(3).x(1,201:300),dataset(3).x(2,201:300),'g*')
+plot(dataset(3).x(1,301:600),dataset(3).x(2,301:600),'c*')
+plot(dataset(3).x(1,601:1000),dataset(3).x(2,601:1000),'m*')
 title("Data Samples")
-clear x1 x2 x3 x4;
 
 % Save this dataset to use for later.
-% save('A3Q1Dataset');
+% save('A3Q2Dataset');
 
+%% GMM fitting with 10 fold Cross Validation
+clear all; close all;
 
+load('A3Q2Dataset.mat');
 % For different number of samples, different numbers of Gaussians will be
 % used to describe it. It Depends on the number of samples, and also their
 % probability distribution.
